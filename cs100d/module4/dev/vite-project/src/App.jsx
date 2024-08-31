054318ef3c60179561420acad6e8e874ff962999
Rebecca Soderberg <vogindex@gmail.com>
2024-08-30 18:54:43 -0700

    I'm trying to make it closer to working!!! No visible errors... But now, for no apparent reason, it's suddenly like, can't open file 'C:\\Users\\rlsod\\rcs1002\\cs100d\\module4\\dev\\vite-project\\run': [Errno 2] No such file or directory!!! The only thing I did was I deleted a second duplicate of the program within the file, kind of, except import './App.css'. Is that confusing? Here, I will save what it was like before I got this error...
    So at this point it's complaining about a duplicate useState, right? Makes sense! The second duplicate is the one that I want, because that's the one that I copied from the tic tac toe tutorial.
    
    See, I restored my old version, but I'm STILL getting the weird vite error!!!

--- cs100d/module4/dev/vite-project/src/App.jsx
+++ cs100d/module4/dev/vite-project/src/App.jsx
@@ -1,12 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-function Main() {
-  /*
-  const [filename, setFilename] = React.useState('Untitled-1.jpg')
-  const [decade, setDecade] = React.useState('1860s')
-  const [title, setTitle] = React.useState('Guess which decade the photo is from')
-  */
+function statestuff() {
   const [state, setState] = React.useState({
     filename: 'Untitled-1.jpg',
     decade:'1860s',
@@ -18,112 +13,223 @@ function Main() {
     count: '', 
     result: ''
 });
-  /*
-    constructor() {
-        super()
-        //Initial data has no user or counts
-        this.urlbase = 'http://localhost:5000'
-        this.state = {rand:0, id: 0, filename:'', decade:'', source:'', info:'', title:'', count: '', result: ''}
-        this.state = {filename: 'Untitled-1.jpg', decade: '1860s', title: 'Guess which decade the photo is from'}
-    }
-  */
     
-    function create_table() {
-        axios.get(this.urlbase + '/create_table').then((resp) => {
-            alert(resp.data)
+function create_table() {
+    axios.get(this.urlbase + '/create_table').then((resp) => {
+        alert(resp.data)
+    })
+}
+
+function login() {
+        const {filename, decade, title, userdecade, count} = this.state
+        var url = '/login'
+        // Store the user's name in a JSON object
+        const body = {'filename': filename}
+        // We're sending JSON data to our server
+        const headers = { "Content-Type": "application/json" }
+        // Configuration information for the server
+        const config = {
+            url: url,
+            baseURL: this.urlbase,
+            method: 'POST',
+            headers: headers,
+            data: body
+        }
+        // Make the request
+    axios(config).then((resp) => {
+        //When this completes, the response from the server has the count data
+        this.setState({...this.state, 
+            filename: resp.data['filename'], // How many logins for this user?
+            decade: resp.data['decade'],
+            title: resp.data['title']
         })
-    }
+    }).catch(error => {
+        console.log(error)
+    })
+}
 
-    function login() {
-            const {filename, decade, title, userdecade, count} = this.state
-            var url = '/login'
-            // Store the user's name in a JSON object
-            const body = {'filename': filename}
-            // We're sending JSON data to our server
-            const headers = { "Content-Type": "application/json" }
-            // Configuration information for the server
-            const config = {
-                url: url,
-                baseURL: this.urlbase,
-                method: 'POST',
-                headers: headers,
-                data: body
-            }
-            // Make the request
-        axios(config).then((resp) => {
-            //When this completes, the response from the server has the count data
-            this.setState({...this.state, 
-                filename: resp.data['filename'], // How many logins for this user?
-                decade: resp.data['decade'],
-                title: resp.data['title']
-            })
-        }).catch(error => {
-            console.log(error)
-        })
-    }
+function arrow() {
+    const {count} = this.state
 
-    function arrow() {
-        const {count} = this.state
-
-        const counter = document.getElementById('counter');
-        const incrementBtn = document.getElementById('increment');
-        const decrementBtn = document.getElementById('decrement');
+    const counter = document.getElementById('counter');
+    const incrementBtn = document.getElementById('increment');
+    const decrementBtn = document.getElementById('decrement');
 /* 
 it doesn't like me changing the const count       
-        incrementBtn.addEventListener('click', () => {
-            count = count + 10;
-            counter.textContent = count;
-        });
-        
-        decrementBtn.addEventListener('click', () => {
-            count = count - 10;
-            counter.textContent = count;
-        });
+      incrementBtn.addEventListener('click', () => {
+          count = count + 10;
+          counter.textContent = count;
+      });
+      
+      decrementBtn.addEventListener('click', () => {
+          count = count - 10;
+          counter.textContent = count;
+      });
 */
-        this.setState({...this.state, 
-            userdecade: count
+    this.setState({...this.state, 
+        userdecade: count
 
-        }).catch(error => {
-            console.log(error)
-        })
-    }
-
-    function create_table() {
-        axios.get(this.urlbase + '/create_table').then((resp) => {
-            alert(resp.data)
-        })
-    }
-
-    function render() {
-        const {filename, decade, title, count} = this.state
-        console.log(filename)
-        const address = (filename) => {
-            return './popdecades/' + filename;
-          }
-        return (
-            <div className='Main'>
-                <div className = 'img'>
-                    <img src = {address(filename)} width="500" height="300"></img>
-                </div>
-
-                <div className = 'container'>
-                    <p>{title}</p>
-                    <button type="button" class = "lrgbutton" onClick={this.login.bind(this)}>New Photo</button>
-                </div>
-
-                <div className="container">
-                    <IncrementDecrementBtn minValue={1840} maxValue={2010} />
-                    <p>count:{count}</p>
-                </div>
-
-                <div className = 'container'>
-                    <SubmitBtn/>
-                </div>
-                
-            </div>
-        )
-    }
+    }).catch(error => {
+        console.log(error)
+    })
 }
+
+function create_table() {
+    axios.get(this.urlbase + '/create_table').then((resp) => {
+        alert(resp.data)
+    })
+}
+const {filename, decade, title, count} = this.state
+console.log(filename)
+const address = (filename) => {
+    return './popdecades/' + filename;
+  }
+
+
+    return (
+        <div className='Main'>
+            <div className = 'img'>
+                <img src = {address(filename)} width="500" height="300"></img>
+            </div>
+
+            <div className = 'container'>
+                <p>{title}</p>
+                <button type="button" class = "lrgbutton" onClick={this.login.bind(this)}>New Photo</button>
+            </div>
+
+            <div className="container">
+                <IncrementDecrementBtn minValue={1840} maxValue={2010} />
+                <p>count:{count}</p>
+            </div>
+
+            <div className = 'container'>
+                <SubmitBtn/>
+            </div>
+            
+        </div>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Main />);
+
+
+
+
+
+
+
+
+import { useState } from 'react';
+
+export default function Main() {
+
+  const [state, setState] = React.useState({
+    filename: 'Untitled-1.jpg',
+    decade:'1860s',
+    title: 'Guess which decade the photo is from',
+    rand:0, 
+    id: 0, 
+    source:'', 
+    info:'', 
+    count: '', 
+    result: ''
+  });
+
+  const {filename, decade, title, count} = this.state
+  console.log(filename)
+  const address = (filename) => {
+    return './popdecades/' + filename;
+  }
+
+  function create_table() {
+    axios.get(this.urlbase + '/create_table').then((resp) => {
+        alert(resp.data)
+    })
+  }
+
+  function login() {
+        const {filename, decade, title, userdecade, count} = this.state
+        var url = '/login'
+        // Store the user's name in a JSON object
+        const body = {'filename': filename}
+        // We're sending JSON data to our server
+        const headers = { "Content-Type": "application/json" }
+        // Configuration information for the server
+        const config = {
+            url: url,
+            baseURL: this.urlbase,
+            method: 'POST',
+            headers: headers,
+            data: body
+        }
+        // Make the request
+    axios(config).then((resp) => {
+        //When this completes, the response from the server has the count data
+        this.setState({...this.state, 
+            filename: resp.data['filename'], // How many logins for this user?
+            decade: resp.data['decade'],
+            title: resp.data['title']
+        })
+    }).catch(error => {
+        console.log(error)
+    })
+  }
+
+  function arrow() {
+    const {count} = this.state
+
+    const counter = document.getElementById('counter');
+    const incrementBtn = document.getElementById('increment');
+    const decrementBtn = document.getElementById('decrement');
+  /* 
+  it doesn't like me changing the const count       
+      incrementBtn.addEventListener('click', () => {
+          count = count + 10;
+          counter.textContent = count;
+      });
+      
+      decrementBtn.addEventListener('click', () => {
+          count = count - 10;
+          counter.textContent = count;
+      });
+  */
+    this.setState({...this.state, 
+        userdecade: count
+
+    }).catch(error => {
+        console.log(error)
+    })
+  }
+
+  function create_table() {
+    axios.get(this.urlbase + '/create_table').then((resp) => {
+        alert(resp.data)
+    })
+  }
+
+
+  return (
+    <div className='Main'>
+        <div className = 'img'>
+            <img src = {address(filename)} width="500" height="300"></img>
+        </div>
+
+        <div className = 'container'>
+            <p>{title}</p>
+            <button type="button" class = "lrgbutton" onClick={this.login.bind(this)}>New Photo</button>
+        </div>
+
+        <div className="container">
+            <IncrementDecrementBtn minValue={1840} maxValue={2010} />
+            <p>count:{count}</p>
+        </div>
+
+        <div className = 'container'>
+            <SubmitBtn/>
+        </div>
+        
+    </div>
+  );
+}
