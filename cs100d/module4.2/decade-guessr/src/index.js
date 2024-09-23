3a968151c464899454ee3953a2562498ba30f446
Rebecca Soderberg <vogindex@gmail.com>
2024-09-23 16:42:58 -0700

    I put the filename for the import in quotation marks!!! that seems a little more correct!!! It's still not working. 'Module not found: Error: Can't resolve 'server.py' in 'C:\Users\rlsod\rcs1002\cs100d\module4.2\decade-guessr\src' Did you mean './server.py'?' Maybe I meant './server.py'.
    Well, now it's complaining about bind again!!! I don't exactly know what that bind thing is doing...

--- cs100d/module4.2/decade-guessr/src/index.js
+++ cs100d/module4.2/decade-guessr/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import create_table from server.py
+import create_table from './server.py';
 
 /*
 from fetch_data import result_row
