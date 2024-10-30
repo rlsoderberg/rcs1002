b0b2657c83171a711d33d47ba79cb2f470f65609
Rebecca Soderberg <vogindex@gmail.com>
2024-10-30 09:12:59 -0700

    FLASK UPLOAD SERVER ERROR, CONFIG KEYS?
    Now let me look at this internal server error!!! I mean, it couldn't be THAT many things. Am I writing the address wrong??? Wait, did I not even have an address in there??? I thought I did!!!
    
    Oh right, I should be looking at my VS Code errors!!! So, it says, 'No such file or directory: '/uploads\\21st-Dino.jpg''. What, am I writing my address wrong? Wherever it is? it has like, a forward slash and then a back slash. omg, how do I know when to use a forward slash and when to use a back slash??? And why does it have two back slashes after uploads?
    
    I tried changing it to a backslash, and now it says ''unicodeescape' codec can't decode bytes in position 1-2: truncated \uXXXX escape'. I tried adding a backslash at the end of uploads, and now it says 'unterminated string literal (detected at line 9).' If possible, that seems more erroneous? I'm going back to the unicode error.
    
    Actually, I'm going back to the one with the forward slash. Because at least then it kind of does something. Oh weird, this time the error is 'Could not build url for endpoint 'download_file' with values ['name']. Did you mean 'upload_file' instead?' OK sure, maybe I did mean upload_file... let me try that... now it just goes right back to No File Chosen. Well, I don't really get what download_file is, at all... wasn't I going to be defining that in one of my config keys? Well, I guess I better figure those out!
    
    I can't find much on config keys, except on the flask tutorial, where I don't think it's explained very well at all!!!

--- cs100d/module4.2/decade-guessr/src/newapp.py
+++ cs100d/module4.2/decade-guessr/src/newapp.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = './uploads'
 
 def allowed_file(filename):
     return '.' in filename and \
