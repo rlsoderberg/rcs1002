c36369f4b1bb37fd7f1d419b17bfe21d852609f6
Rebecca Soderberg <vogindex@gmail.com>
2024-10-29 20:55:21 -0700

    STARTING TO VANILLA
    I was trying to think of a way I could do a plain old vanilla 'vanilla flask html form upload image'... how about I just use this tutorial on the Flask website!!! Ooooh!!! They're also using werkzeug, which luckily I already have!!! Look, here's where I'm getting it! 'https://flask.palletsprojects.com/en/stable/patterns/fileuploads/'! Nice!!!
    
    They're talking about making some THING to ' serve the uploaded files so they can be downloaded by users'. Like, what kind of THING??? I don't really get this, so I am skipping for now... I'm also skipping MAX_CONTENT_LENGTH... now, after that, the tutorial ends pretty abruptly. Like, is this thing going to work??? What kinda HTML am I going to use??? I mean, I guess this Flask app doesn't really reference any HTML files. That is awesome!!! Now, let's see how it runs...
    
    Oh right!!! I forgot to put the app-runny thing at the bottom!!! Also, the return is commented out for some reason... I mean... it looks like there's some HTML in there... um... so, how did the other ones link the Python and the HTML again???
    
    OK, so first they go 'return render_template('index.html')'.
    
    So I tried uploading an image, and still internal server error!!! Aaaaa!!! That is my thing I will have to figure out... honestly, with how short this tutorial is, I could at least try to implement MAX_CONTENT_LENGTH, although I still don't know what kind of download THING they want me to make...
    
    This seems to be yet another THING. This time, they described a THING as a 'config key'... let me see what that is...
    
    I'm reading 'https://flask.palletsprojects.com/en/stable/config/'.
    Uhhh... maybe you go like 'app.config['TESTING'] = True' at the top? And, if it's not true... then what happens? And how are we establishing what TESTING is? Wait, TESTING looks like it's a default config key. Oh OK, you can config from an object. Like, an object meaning another Python file?
    
    I guess I will wait on this one. Because I don't know if I should be using one of these? 'app.config.from_object('yourapplication.default_settings')
    app.config.from_envvar('YOURAPPLICATION_SETTINGS')' Like, the first one has a period in it, and I'm like, where are you getting that period from, and the second one doesn't even have a file extension... so, it appears that neither of those lines refer to other Python files, so I still don't know what kind of THING I'm making!!!

--- cs100d/module4.2/decade-guessr/src/newapp.py
+++ cs100d/module4.2/decade-guessr/src/newapp.py
@@ -1,50 +1,35 @@
-#NEWapp.py
-from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
 import os
+from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
- 
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
- 
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
- 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
- 
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
- 
-@app.route('/')
-def home():
-    return render_template('newindex.html')
- 
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('newindex.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
- 
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
- 
-if __name__ == "__main__":
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run()
