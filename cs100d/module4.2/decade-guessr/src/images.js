d881013114f6db033dd589b8524a5930ccfbfd8a
Rebecca Soderberg <vogindex@gmail.com>
2024-11-14 07:52:58 -0800

    DELETING OLD FILES
    Updated image links, deleted a couple old projects I'm not using... and now let me see if I can find a good way to strip...
    
    See, right now the app isn't working, and I can't tell if it's because of something I did!!!  I discarded my changes, but that didn't help...
    
    How annoying!!! I'm just going to... delete all my newest projects, and then make a redo everything, I guess...

--- cs100d/module4.2/decade-guessr/src/images.js
+++
@@ -1,8 +0,0 @@
-const IMAGES = {
-    y1843: require ('./photos/1843.jpg').default,
-    y1855: require ('./photos/1855.jpg').default,
-    y1862: require ('./photos/1862.jpg').default,
-    nullo: require ('./photos/null.jpg').default
-}
-
-export default IMAGES;
--- cs100d/module4.4/image-slider-demo/src/read_data.py
+++
@@ -1,4 +0,0 @@
-#read image data
-data_file = open("img_data.txt", "r", encoding='utf-8')
-lines = data_file.read().splitlines()
-data_file.close()
